29c07ac763f8a119c8387854a4dd39d2c838bf84
diebels727 <diebelsalternative@hotmail.com>
2014-12-09 14:22:16 -0500

    initialize world.

--- swallows.py
+++ swallows.py
@@ -3,6 +3,7 @@
 import sys
 import numpy
 import pdb
+import copy
 
 structures = {}
 graph = {}
@@ -16,42 +17,21 @@ def init_structures():
   structures['path'][0] = []
 
 def next_static_path(v):
-  static_paths = [x for x in structures['starts'] if x >= v]
+  static_paths = [x for x in structures['keys'] if x > v]
   if len(static_paths) == 0:
     return None
   w = static_paths[0]
   cost = (w-v)*structures['cost_unit']
   return [v,w,cost,'static']
 
-def init_graph(vertex):
-  if vertex == None:
-    return
-  if visited.get(vertex):
-    return
-
-  visited[vertex] = True
-
-  flight_paths = structures['flight_paths']
-  flight_path = flight_paths.get(vertex)
-
-  static_path = next_static_path(vertex)
-
-  print static_path
-
-  graph[vertex] = (graph.get(vertex) or [])
-
-  if (static_path==None) and (flight_path==None):
-    return
-
-  if static_path:
-    graph[vertex].append(static_path)
-  if flight_path:
-    graph[vertex].append(flight_path)
-
-  if static_path:
-    init_graph(static_path[1])
-  if flight_path:
-    init_graph(flight_path[1])
+def init_graph(s):
+  global graph
+  keys = [0] + structures['keys']
+  for key in keys:
+    static_path = next_static_path(key)
+    graph[key] = graph.get(key) or []
+    graph[key].append(static_path)
+    # graph[key] = (graph.get(key) or []).append(static_path)
 
 def main(filename):
   init_structures()
@@ -59,7 +39,13 @@ def main(filename):
   starts = structures['flight_paths'].keys()
   starts.sort()
   structures['starts'] = starts
+
+  keys = graph.keys()
+  keys.sort()
+  structures['keys'] = keys
+
   init_graph(0)
+
   print graph
 
 
@@ -72,7 +58,11 @@ def index_flight_paths(filename):
   end = 0
   for line in fh:
     s,t,cost,path_type = normalize_path_data(line)
-    flight_paths[s] = (flight_paths.get(s) or [s,t,cost,path_type])
+    flight_paths[s] = (flight_paths.get(s) or [])
+    flight_paths[s].append([s,t,cost,path_type])
+    graph[s] = (graph.get(s) or [])
+    graph[s].append([s,t,cost,path_type])
+    graph[t] = (graph.get(t) or [])
     if t > end:
       end = t
   fh.close()
