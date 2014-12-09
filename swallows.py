80027461a46cdccffc6079ca0508b48f898dba67
diebels727 <diebelsalternative@hotmail.com>
2014-12-08 20:05:41 -0500

    read file

--- swallows.py
+++ swallows.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python
 
 import sys
+
+def main(filename):
+  fh = open(filename, "r" )
+  array = []
+  for line in fh:
+      line = line.strip()
+      print line
+      array.append( line )
+  fh.close()
+
+if __name__ == "__main__":
+  main(sys.argv[1])
