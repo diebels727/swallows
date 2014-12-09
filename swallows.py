ee3384f3a69cd8ce6f9b795afd1b78cfa21a89be
diebels727 <diebelsalternative@hotmail.com>
2014-12-08 20:46:25 -0500

    load up file.

--- swallows.py
+++ swallows.py
@@ -1,19 +1,39 @@
 #!/usr/bin/python
 
 import sys
+import numpy
 
 def main(filename):
-  content = read_file(filename)
+  cost_unit,flight_paths = index_flight_paths(filename)
+  print cost_unit
+  print flight_paths
+  # cost_per_mile = content[0]
+  # flight_paths = content[1:]
 
-def read_file(filename):
+  #index flight paths
+
+
+def index_flight_paths(filename):
   fh = open(filename, "r" )
-  array = []
+  cost_str = fh.readline()
+  cost_unit = int(cost_str)
+  flight_paths = {}
   for line in fh:
-      line = line.strip()
-      array.append( line )
+    s,t,cost = normalize_path_data(line)
+    flight_paths[s] = (flight_paths.get(s) or [s,t,cost])
+  return cost_unit,flight_paths
+
+
   fh.close()
   return array
 
+def normalize_path_data(line):
+  # line.strip()
+  tuples = line.split()
+  s = int(tuples[0])
+  t = int(tuples[1])
+  cost = int(tuples[2])
+  return s,t,cost
 
 if __name__ == "__main__":
   main(sys.argv[1])
