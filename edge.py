310322dc233d21b250e18c925a4265f1dd86d736
diebels727 <diebelsalternative@hotmail.com>
2014-12-10 08:46:38 -0500

    Effectively killed my graph.

--- edge.py
+++ edge.py
@@ -4,3 +4,16 @@ class Edge:
     self.t = s
     self.w = w
     self.type = et
+
+  def is_null():
+    return False
+
+class NullEdge:
+  def __init__(self):
+    self.s = None
+    self.t = None
+    self.w = None
+    self.type = None
+
+  def is_null():
+    return True
